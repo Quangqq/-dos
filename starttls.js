7e02330e150ac2ed2bdf56716821e4f7f4583656
Pham Huỳnh Quang <141423816+Quangqq@users.noreply.github.com>
2025-01-26 11:22:35 +0700

    Create starttls.js

---
+++ starttls.js
@@ -0,0 +1,21 @@
+const { parentPort, workerData } = require('worker_threads');
+const { spawn } = require('child_process');
+
+// Nhận dữ liệu từ workerData
+const { url, time, rate, thea, proxy } = workerData;
+
+// Hàm chạy flooder.js
+function runFlooder() {
+    const flooder = spawn('node', ['tls.js', url, time, rate, thea, proxy]);
+
+    flooder.stdout.on('data', (data) => {
+        parentPort.postMessage(`Output: ${data}`);
+    });
+
+    flooder.stderr.on('data', (data) => {
+        parentPort.postMessage(`Error: ${data}`);
+    });
+}
+
+// Chạy flooder
+runFlooder();
