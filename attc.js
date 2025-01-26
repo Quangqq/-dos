027128a53498429ff03f5b251929585db4ce439d
Pham Huỳnh Quang <141423816+Quangqq@users.noreply.github.com>
2025-01-26 16:07:03 +0700

    Update attc.js

--- attc.js
+++ attc.js
@@ -584,7 +584,7 @@ function getRandomUserAgent() {
  
  const secureContext = tls.createSecureContext(secureContextOptions);
  
- var proxyFile = "proxy.txt";
+ var proxyFile = "proxies.txt";
  var proxies = readLines(proxyFile);
  var userAgents = readLines("ua.txt");
  
