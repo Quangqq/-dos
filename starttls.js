e27458ef6a0f3d771089b5ed49a3c9cc89eb033e
Pham Huỳnh Quang <141423816+Quangqq@users.noreply.github.com>
2025-01-30 11:55:46 +0700

    Update starttls.js

--- starttls.js
+++ starttls.js
@@ -4,16 +4,32 @@ const { spawn } = require('child_process');
 // Nhận dữ liệu từ workerData
 const { url, time, rate, thea, proxy } = workerData;
 
+// Chế độ tấn công (flood hoặc bypass)
+const mode = 'flood'; 
+
 // Hàm chạy flooder.js
 function runFlooder() {
-    const flooder = spawn('node', ['new.js', url, time, rate, thea, proxy, flood]);
+    const flooder = spawn('node', ['new.js', url, time, rate, thea, proxy, mode]);
 
     flooder.stdout.on('data', (data) => {
-        parentPort.postMessage(`Output: ${data}`);
+        parentPort.postMessage(`Output: ${data.toString()}`);
     });
 
     flooder.stderr.on('data', (data) => {
-        parentPort.postMessage(`Error: ${data}`);
+        parentPort.postMessage(`Error: ${data.toString()}`);
+    });
+
+    flooder.on('exit', (code) => {
+        parentPort.postMessage(`Flooder Success!: ${code}`);
+        parentPort.close(); // Dừng worker khi xong
+    });
+
+    // Lắng nghe tín hiệu dừng worker
+    parentPort.on('message', (msg) => {
+        if (msg === 'stop') {
+            flooder.kill();
+            parentPort.close();
+        }
     });
 }
 
